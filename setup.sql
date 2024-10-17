021b43f30955e4d675ce6d45ab30cd262dd10368
stevenbehrens <stevenbehrens3@gmail.com>
2024-10-17 19:39:56 -0400

    Creating Table for user information

--- setup.sql
+++ setup.sql
@@ -2,3 +2,12 @@
 DROP DATABASE IF EXISTS `password_manager`;
 CREATE DATABASE `password_manager` DEFAULT CHARACTER SET utf8mb4;
 USE `password_manager`;
+
+CREATE TABLE IF NOT EXISTS user_information (
+    database_id        SMALLINT        NOT NULL AUTO_INCREMENT,
+    first_name      VARCHAR(20)        NOT NULL,
+    last_name       VARCHAR(20)        NOT NULL,
+    email           VARCHAR(50)        NOT NULL,
+    notes           VARCHAR(500)               ,
+    PRIMARY KEY (database_id)
+);
