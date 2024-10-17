ec3156ffaf8cdbe83633991201da3acfeaa6bad8
stevenbehrens <stevenbehrens3@gmail.com>
2024-10-17 17:12:00 -0400

    inserting website values

--- commands.sql
+++ commands.sql
@@ -8,7 +8,11 @@ CREATE PROCEDURE INSERT_OR_UPDATE(
    IN password VARCHAR(30),
    IN comments VARCHAR(1500)
    )
+   
 BEGIN
 INSERT INTO user (first_name, last_name, email)
 VALUES (first_name, last_name, email)
 ON DUPLICATE KEY UPDATE first_name = VALUES(first_name), last_name = VALUES(last_name);
+INSERT INTO website (website_name, website_url)
+VALUES (website_name, website_url)
+ON DUPLICATE KEY UPDATE website_name = VALUES(website_name)
