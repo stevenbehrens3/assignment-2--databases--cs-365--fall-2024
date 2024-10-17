9faf9ccfd7e616ddf8e3c432f7d83947df8367c6
stevenbehrens <stevenbehrens3@gmail.com>
2024-10-17 17:07:57 -0400

    Created procedure for user

---
+++ commands.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE INSERT_OR_UPDATE(
+   IN website_name VARCHAR(150),
+   IN website_url VARCHAR(500),
+   IN first_name VARCHAR(30),
+   IN last_name VARCHAR(30),
+   IN username VARCHAR(30),
+   IN email VARCHAR(50),
+   IN password VARCHAR(30),
+   IN comments VARCHAR(1500)
+   )
